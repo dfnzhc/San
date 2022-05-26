43de5bb2ac95feb6d8dea37a9a65c4173ec617cd
su <z69500jqk@qq.com>
2022-05-26 18:08:52 +0800

    增加一些工具函数

---
+++ source/yuan/platform/filesystem.hpp
@@ -0,0 +1,14 @@
+﻿//
+// Created by 秋鱼 on 2022/5/26.
+//
+
+#pragma once
+
+namespace Yuan {
+
+inline std::string_view GetFileExtension(std::string_view fileName)
+{
+    return fileName.substr(fileName.rfind('.')).substr(1);
+}
+
+} // namespace Yuan
