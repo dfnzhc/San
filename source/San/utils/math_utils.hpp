b77dbc79006d255ce6d1e542a92b00140df2571e
su <z69500jqk@qq.com>
2022-06-14 17:22:12 +0800

    一些数学工具

---
+++ source/San/utils/math_utils.hpp
@@ -0,0 +1,15 @@
+﻿//
+// Created by 秋鱼 on 2022/6/14.
+//
+
+#pragma once
+
+namespace San {
+
+template<typename T>
+inline T AlignUp(T val, T alignment)
+{
+    return (val + alignment - static_cast<T>(1)) & ~(alignment - static_cast<T>(1));
+}
+
+} // namespace San
