4a17dce92bbe64754b893372b04d82d95c8e2383
su <z69500jqk@qq.com>
2022-06-22 18:31:54 +0800

    添加几个工具函数

--- source/San/utils/utils.cpp
+++ source/San/utils/utils.cpp
@@ -6,10 +6,31 @@
 
 namespace San {
 
-
 std::string_view GetFileExtension(std::string_view fileName)
 {
     return fileName.substr(fileName.rfind('.')).substr(1);
+}
+
+std::string_view GetFileName(std::string_view fullName)
+{
+    auto base = fullName.substr(fullName.find_last_of("/\\") + 1);
+
+    const auto p = base.find_last_of('.');
+    if (p > 0 && p != std::string::npos) {
+        base = base.substr(0, p);
+    }
+
+    return base;
+}
+
+bool IsFileExist(std::string_view fileName)
+{
+    //return std::filesystem::exists(fileName);
+
+    // fast check file if exists
+    // https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exists-using-standard-c-c11-14-17-c
+    struct stat buffer;
+    return (stat(fileName.data(), &buffer) == 0);
 }
 
 } // namespace San
--- source/San/utils/utils.hpp
+++ source/San/utils/utils.hpp
@@ -8,4 +8,8 @@ namespace San {
 
 std::string_view GetFileExtension(std::string_view fileName);
 
+std::string_view GetFileName(std::string_view fullName);
+
+bool IsFileExist(std::string_view fileName);
+
 } // namespace San
